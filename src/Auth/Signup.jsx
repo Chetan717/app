c89c7027bddcbbd308f14f5735a524eccb937b4e
hsoft7 <harisoftwares7@gmail.com>
2026-03-16 19:40:38 +0530

    mlmbooster

--- src/Auth/Signup.jsx
+++ src/Auth/Signup.jsx
@@ -63,7 +63,7 @@ export function Signup() {
       setLoading(true);
       setFormError("");
 
-      const response = await axios.post(SIGNUP_URL, payload);
+      const response = await axios.put(SIGNUP_URL, payload);
 
       console.log("API Response:", response.data);
 
